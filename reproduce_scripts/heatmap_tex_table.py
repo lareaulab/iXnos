d91dc270fcb9a2ab126001e4277744fe7e6ad09d
Robert Tunney <robert.tunney@gmail.com>
2017-09-24 02:54:41 -0700

    added file to format a heatmap scores table for latex

---
+++ reproduce_scripts/heatmap_tex_table.py
@@ -0,0 +1,27 @@
+import sys
+
+def make_heatmap_tex_table(in_fname, out_fname):
+    in_file = open(in_fname, "r")
+    out_file = open(out_fname, "w")
+    dt = []
+    for line in in_file:
+        line = line.strip().split()
+        line = [float(elt) for elt in line]
+        line = [round(elt, 4) for elt in line]
+        dt.append(line)
+    alpha = "ACGT"
+    cods = [x + y + z for x in alpha for y in alpha for z in alpha]
+    stops = ["TAA", "TAG", "TGA"]
+    header ="\t" + "\t".join([str(elt) for elt in range(-7,6)]) + "\n"
+    out_file.write(header)
+    for idx, cod in enumerate(cods):
+        if cod not in stops:
+            line = "\t".join([cod] + [str(elt) for elt in dt[idx]]) + "\n"
+            out_file.write(line)
+    in_file.close()
+    out_file.close()
+
+if __name__ == "__main__":
+    in_fname = sys.argv[1]
+    out_fname = sys.argv[2]
+    make_heatmap_tex_table(in_fname, out_fname)
